13ab03713e2e3da90b1be5cf702c06ff9658cb9d
EduOceans <150608334+EduOceans@users.noreply.github.com>
2024-03-17 20:41:59 +0000

    adding style to anouncement component

---
+++ src/components/history/History.jsx
@@ -0,0 +1,11 @@
+import './History.css';
+
+function History() {
+    return (
+        <div className="history-txt">
+            <p> Founded in the early 1900s by visionary Amelia Sinclair, Bahama Sands has evolved from a humble guesthouse to a renowned beachfront retreat. With its iconic seashell-shaped architecture and lush gardens, this coastal oasis offers a timeless escape into luxury. Today, Bahama Sands stands as a beacon of relaxation and sophistication, inviting guests to create cherished memories amidst the beauty of the Bahamas.</p>
+        </div>
+    )
+}
+
+export default History;
