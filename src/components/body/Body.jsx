75a43a79595ebd62c0a25f93ec249cb945c1cb09
EduOceans <150608334+EduOceans@users.noreply.github.com>
2024-03-18 11:48:27 +0000

    adding components and styling

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,7 +1,8 @@
 import ImageButton from "../imageButton/ImageButton"
 import './Body.css';
-import Image from "../image/Image";
 import Anouncement from "../anouncement/Anouncement";
+import About from "../about/About";
+import History from "../history/History";
 
 function Body() {
     return (
@@ -18,16 +19,8 @@ function Body() {
                         subHeading={"Wherever you are in the world, book now one of our suites"}
                         btnTxt={"Book"}/>
             </section>
-            <Anouncement />
-            <section>
-                <div className="intro-txt">
-                    <p> Welcome to Suitestay, your premier destination for unparalleled luxury and comfort. Nestled in the heart of bustling city life or surrounded by serene natural landscapes, our collection of boutique hotels offers a distinctive blend of sophistication and warmth.
-                        At Suitestay, we understand that every guest is unique, which is why we pride ourselves on providing personalized experiences tailored to your desires. Whether you're seeking a romantic escape, a family adventure, or a productive business trip, our attentive staff is dedicated to exceeding your expectations at every turn.
-                        Indulge in our meticulously designed suites, each thoughtfully curated to offer the ultimate in relaxation and refinement. From plush bedding and modern amenities to panoramic views and bespoke furnishings, every detail has been carefully considered to ensure your stay is nothing short of extraordinary.
-                        Beyond the comfort of your suite, Suitestay invites you to savor exquisite culinary delights at our renowned restaurants, unwind with a rejuvenating spa treatment, or explore the vibrant local culture with our expertly curated excursions.
-                        Experience the epitome of luxury hospitality with Suitestay, where every moment is crafted with your utmost satisfaction in mind. Welcome to a world of elegance, where unforgettable memories await.</p>
-                </div>
-            </section>
+            <About />
+            <History />
             <section>
                 Icons
             </section>
--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -2,8 +2,27 @@ import './History.css';
 
 function History() {
     return (
-        <div className="history-txt">
-            <p> Founded in the early 1900s by visionary Amelia Sinclair, Bahama Sands has evolved from a humble guesthouse to a renowned beachfront retreat. With its iconic seashell-shaped architecture and lush gardens, this coastal oasis offers a timeless escape into luxury. Today, Bahama Sands stands as a beacon of relaxation and sophistication, inviting guests to create cherished memories amidst the beauty of the Bahamas.</p>
+        <div className='history-container'>
+            <div className='history-title'>
+                <h1>Infinite harmony</h1>
+            </div>
+            <div className="history-txt">
+                <h3> Founded in the early 1900s, Suitestay has evolved from a humble guesthouse to a renowned beachfront retreat. With its iconic sea views and lush gardens, this coastal oasis offers a timeless escape into luxury. Today, Suitestay stands as a beacon of relaxation and sophistication, inviting guests to create cherished memories amidst the beauty of the Bahamas.</h3>
+            </div>
+            <div className='rooftop-container'>
+                <div className='text-container'>
+                    <h5 className='rooftop-heading'>ROOFTOP</h5>
+                    <h2 className='rooftop-title'>AWARD WINNING PARADISE</h2>
+                    <p className='rooftop-text'>At our Sea View Rooftop, indulge in panoramic ocean vistas. Savor drinks with the sea as your backdrop, embracing tranquility and natural beauty.</p>
+                </div>
+                <div className='cocktail-container'>
+                <img className='cocktail' src='src/assets/cocktail.jpeg'alt='cocktail'></img>
+                </div>
+                <div className='cheers-container'>
+                <img src='src/assets/cheers.jpeg' className='cheers' alt='cheers'></img>
+                </div>
+            </div>
+            
         </div>
     )
 }
