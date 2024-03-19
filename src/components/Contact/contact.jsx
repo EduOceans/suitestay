eb7ee47afd7b629024c769fe46b8d309c949ff35
KaleimS <151700396+KaleimS@users.noreply.github.com>
2024-03-19 20:25:58 +0000

    Updated UI and add prime toast for submit (#31)

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,72 +1,42 @@
-import React, { useState } from "react";
-
-import { Form, Field } from "react-final-form";
-
+import React, { useRef, useState } from "react";
+import { Toast } from "primereact/toast";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 
-import { InputTextarea } from "primereact/inputtextarea";
-
-import { Button } from "primereact/button";
-
-import { Dialog } from "primereact/dialog";
-
-import { classNames } from "primereact/utils";
-
-import "./contact.css"
-
 export default function Contact() {
-  const [showMessage, setShowMessage] = useState(false);
-  const [formData, setFormData] = useState({});
+  const toast = useRef(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const onSubmit = (data, form) => {
-    setFormData(data);
-    setShowMessage(true);
-
-    form.restart();
+  const showEmptyFieldsToast = () => {
+    toast.current.show({
+      severity: "warn",
+      summary: "Warning",
+      detail: "Please fill in all of the fields.",
+    });
+  };
+  const showSuccessToast = () => {
+    toast.current.show({
+      severity: "success",
+      summary: "Success",
+      detail: "Message sent successfully!",
+    });
   };
 
-  const validate = (data) => {
-    let errors = {};
-
-    if (!data.name) {
-      errors.name = "Name is required.";
+  const handleSubmit = () => {
+    if (!name || !email || !message) {
+      showEmptyFieldsToast();
+    } else {
+      showSuccessToast();
     }
-
-    if (!data.email) {
-      errors.email = "Email is required.";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)) {
-      errors.email = "Invalid email address. E.g. example@email.com";
-    }
-
-    if (!data.message) {
-      errors.message = "Please input your message here."
-    }
-
-    return errors;
   };
-
-  const isFormFieldValid = (meta) => !!(meta.touched && meta.error);
-  const getFormErrorMessage = (meta) => {
-    return (
-      isFormFieldValid(meta) && <small className="p-error">{meta.error}</small>
-    );
-  };
-
-  const dialogFooter = (
-    <div className="flex justify-content-center">
-      <Button
-        label="OK"
-        className="p-button-text"
-        autoFocus
-        onClick={() => setShowMessage(false)}
-      />
-    </div>
-  );
 
   return (
-    <div>
+    <div className="">
       <div className="surface-0">
-        <div className="text-900 font-bold text-6xl mb-4 text-center">
+        <div className="text-900 font-bold text-6xl mb-4 text-center pt-5 border-top-3">
           GET IN TOUCH WITH US
         </div>
         <div className="text-700 text-xl mb-6 text-center line-height-3">
@@ -74,11 +44,11 @@ export default function Contact() {
           say hello!
         </div>
 
-        <div className="grid ">
-          <div className="col-12 lg:col-4 ">
-            <div className="p-3 h-full ">
+        <div className="grid">
+          <div className="col-12 lg:col-4">
+            <div className="p-3 h-full">
               <div
-                className="shadow-2 p-3 h-full flex flex-column border-solid border-white-300 border-round-xl text-center"
+                className="shadow-4 p-3 h-full flex flex-column border-round-xl text-center"
                 style={{ borderRadius: "6px" }}
               >
                 <div className="text-900 font-medium text-xl mb-2">
@@ -94,19 +64,19 @@ export default function Contact() {
                 </div>
                 <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300 border-white-400" />
                 <div className="text-center">
-                  <i class="pi pi-envelope pr-2"></i>
+                  <i className="pi pi-envelope pr-2"></i>
                   <span className="">Email: </span>
                   <a href="mailto:Suitestay@gmail.com">Suitestay@gmail.com</a>
                 </div>
                 <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300 border-white-400" />
                 <div className="text-center">
-                  <i class="pi pi-compass pr-2"></i>
+                  <i className="pi pi-compass pr-2"></i>
                   <span className="">Find Us: </span>
                   <a href="tel://01234567890"></a>123 Main Street
                 </div>
                 <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300 border-white-400" />
                 <div className="text-center">
-                  <i class="pi pi-calendar-times pr-2"></i>
+                  <i className="pi pi-calendar-times pr-2"></i>
                   <span className="">Opening Hours: </span>
                   <span>24/7</span>
                 </div>
@@ -118,7 +88,7 @@ export default function Contact() {
           <div className="col-12 lg:col-8">
             <div className="p-3 h-full">
               <div
-                className="shadow-2 p-3 h-full flex flex-column border-solid border-white-300 border-round-xl text-center"
+                className="shadow-4 p-3 h-full flex flex-column border-round-xl text-center"
                 style={{ borderRadius: "6px" }}
               >
                 <div className="text-900 font-medium text-xl mb-2">
@@ -126,124 +96,48 @@ export default function Contact() {
                 </div>
                 <div className="text-600">Get in touch</div>
                 <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300 border-white-300" />
-                <div>
-                  <Dialog
-                    visible={showMessage}
-                    onHide={() => setShowMessage(false)}
-                    position="top"
-                    footer={dialogFooter}
-                    showHeader={false}
-                    breakpoints={{ "960px": "80vw" }}
-                    style={{ width: "30vw" }}
-                  >
-                    <div className="flex align-items-center flex-column pt-6 px-3">
-                      <i
-                        className="pi pi-check-circle"
-                        style={{ fontSize: "5rem", color: "var(--green-500)" }}
-                      ></i>
-                      <h5>Registration Successful!</h5>
-                      <p style={{ lineHeight: 1.5, textIndent: "1rem" }}>
-                        Your account is registered under name{" "}
-                        <b>{formData.name}</b> ; it'll be valid next 30 days
-                        without activation. Please check <b>{formData.email}</b>{" "}
-                        for activation instructions.
-                      </p>
-                    </div>
-                  </Dialog>
-                  <Form
-                    onSubmit={onSubmit}
-                    initialValues={{
-                      name: "",
-                      email: "",
-                    }}
-                    validate={validate}
-                    render={({ handleSubmit }) => (
-                      <form onSubmit={handleSubmit} className="p-fluid">
-                        <Field
-                          name="name"
-                          render={({ input, meta }) => (
-                            <div className="field">
-                              <span className="p-float-label p-input-icon-right">
-                              <i className="pi pi-id-card" />
-                                <InputText
-                                  id="name"
-                                  {...input}
-                                  autoFocus
-                                  className={classNames({
-                                    "p-invalid": isFormFieldValid(meta),
-                                  })}
-                                />
-                                <label
-                                  htmlFor="name"
-                                  className={classNames({
-                                    "p-error": isFormFieldValid(meta),
-                                  })}
-                                >
-                                  Name*
-                                </label>
-                              </span>
-                              {getFormErrorMessage(meta)}
-                            </div>
-                          )}
-                        />
-                        <Field
-                          name="email"
-                          render={({ input, meta }) => (
-                            <div className="field">
-                              <span className="p-float-label p-input-icon-right">
-                                <i className="pi pi-envelope" />
-                                <InputText
-                                  id="email"
-                                  {...input}
-                                  className={classNames({
-                                    "p-invalid": isFormFieldValid(meta),
-                                  })}
-                                />
-                                <label
-                                  htmlFor="email"
-                                  className={classNames({
-                                    "p-error": isFormFieldValid(meta),
-                                  })}
-                                >
-                                  Email*
-                                </label>
-                              </span>
-                              {getFormErrorMessage(meta)}
-                            </div>
-                          )}
-                        />
-                        <Field
-                          name="message"
-                          render={({ input, meta }) => (
-                            <div className="field">
-                              <span className="p-float-label p-input-icon-right">
-                                <i className="pi pi-comment" />
-                                <InputTextarea
-                                  id="message"
-                                  {...input}
-                                  className={classNames({
-                                    "p-invalid": isFormFieldValid(meta),
-                                  })}
-                                />
-                                <label
-                                  htmlFor="message"
-                                  className={classNames({
-                                    "p-error": isFormFieldValid(meta),
-                                  })}
-                                >
-                                  Message*
-                                </label>
-                              </span>
-                              {getFormErrorMessage(meta)}
-                            </div>
-                          )}
-                        />
-                        <Button type="submit" label="Submit" className="mt-2 text-white border-white" />
-                      </form>
-                    )}
+                <form className="p-fluid">
+                  <div className="field pt-2">
+                    <span className="p-float-label p-input-icon-right">
+                      <i className="pi pi-id-card" />
+                      <InputText
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                      <label htmlFor="name">Name*</label>
+                    </span>
+                  </div>
+                  <div className="field pt-2">
+                    <span className="p-float-label p-input-icon-right">
+                      <i className="pi pi-envelope" />
+                      <InputText
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <label htmlFor="email">Email*</label>
+                    </span>
+                  </div>
+                  <div className="field pt-2">
+                    <span className="p-float-label p-input-icon-right">
+                      <i className="pi pi-comment" />
+                      <InputTextarea
+                        id="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                      />
+                      <label htmlFor="message">Message*</label>
+                    </span>
+                  </div>
+                  <Toast ref={toast} />
+                  <Button
+                    onClick={handleSubmit}
+                    type="button"
+                    label="Submit"
+                    className="mt-2"
                   />
-                </div>
-
+                </form>
                 <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
               </div>
             </div>
