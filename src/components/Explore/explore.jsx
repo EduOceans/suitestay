041d20a1a8d6fe769d4a9962e2f567c2e7b7af9c
Vivek Iyer <vivek370@gmail.com>
2024-03-20 20:23:45 +0000

    ExploreTab

--- src/components/Explore/explore.jsx
+++ src/components/Explore/explore.jsx
@@ -5,17 +5,12 @@ import Restaurants from '../restaurants/restaurants';
 
 export default function BasicDemo() {
     return (
-        <div className="card">
-            <TabView>
-                <TabPanel header="Places To See NearBy">
-                    // Add the content to display map and places to see
-
-                </TabPanel>
-                <TabPanel header="Restruants NearBy">
-                    <Restaurants />
-                </TabPanel>
-            </TabView>
+        <div className="surface-0">
+           <div className="text-center">
+            Hello
+            </div>
         </div>
+
     )
 }
         
