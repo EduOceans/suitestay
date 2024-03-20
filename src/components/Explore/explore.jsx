5b4aed8f915f8ce377a407be6bf7dfd4f555d6cd
Vivek Iyer <vivek370@gmail.com>
2024-03-20 20:49:41 +0000

    ExploreTab

--- src/components/Explore/explore.jsx
+++ src/components/Explore/explore.jsx
@@ -1,16 +1,20 @@
 import React from 'react'; 
 import { TabView, TabPanel } from 'primereact/tabview';
-
 import Restaurants from '../restaurants/restaurants';
 
 export default function BasicDemo() {
     return (
-        <div className="surface-0">
-           <div className="text-center">
-            Hello
-            </div>
-        </div>
+        <div className="card">
+            <TabView>
+                <TabPanel header="Places To See NearBy">
+                    // Add the content to display map and places to see
 
+                </TabPanel>
+                <TabPanel header="Restruants NearBy">
+                    <Restaurants />
+                </TabPanel>
+            </TabView>
+        </div>
     )
 }
         
