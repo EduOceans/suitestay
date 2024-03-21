b107926bb8797c3d4608cf2d49eebc17090442c7
EduOceans <150608334+EduOceans@users.noreply.github.com>
2024-03-21 13:34:03 +0200

    booking component

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,109 +1,55 @@
+import Bookings from "../bookings/Bookings";
 import ImageButton from "../imageButton/ImageButton";
 import "./Body.css";
-import { Button } from "primereact/button";
 
 function Body() {
   return (
     <div>
-      <div className="surface-0">
-        <div className="grid justify-content-around overflow-hidden pt-5">
-            <ImageButton 
-                imgUrl={"https://assets.website-files.com/64be07bb1f833c7c8821b67c/64bfda83ce7792ff7034126a_DeluxeRoom.jpg"}
-                heading={"£300/night"}
-                subHeading={"Deluxe room"}
-            /> 
-            <ImageButton 
-                imgUrl={"https://assets.website-files.com/64be07bb1f833c7c8821b67c/64bfe055d6a432f7de96be21_PenthouseSuite.jpg"}
-                heading={"£450/night"}
-                subHeading={"Ocean view suite"}
-            /> 
-            <ImageButton 
-                imgUrl={"https://assets.website-files.com/64be07bb1f833c7c8821b67c/64bfdca47f54ac3ff269a282_PremierSuite2.jpg"}
-                heading={"£670/night"}
-                subHeading={"Premier suite"}
-            /> 
-          {/* <div className="col-12 md:col-4 lg:col-4 pt-2">
-            <div className="text-center border-round-sm font-bold pt-5">
-              <img
-                src=""
-                className="img-fluid border-round-xl"
-                alt=""
-                height={275}
-              />
-              <div className="text-2xl font-normal pt-5">Gallery</div>
-              <div className="font-light pt-4">
-                You can browse our wonderful Suites
-              </div>
-              <div className="pt-5">
-                <Button label="View Suites" outlined />
-              </div>
-            </div>
-          </div>
-          <div className="col-12 md:col-4 lg:col-4 pt-2">
-            <div className="text-center border-round-sm font-bold pt-5">
-              <img
-                src=""
-                className="img-fluid border-round-xl"
-                alt=""
-                height={275}
-              />
-              <div className="text-2xl font-normal pt-5">
-                Check Availability
-              </div>
-              <div className="font-light pt-4">
-                Get ready for the trip of a lifetime
-              </div>
-              <div className="pt-5">
-                <Button label="Book" outlined />
-              </div>
-            </div>
-          </div>
-          <div className="col-12 md:col-4 lg:col-4 pt-2">
-            <div className="text-center border-round-sm font-bold pt-5">
-              <img
-                src=""
-                className="img-fluid border-round-xl"
-                alt=""
-                height={275}
-              />
-              <div className="text-2xl font-normal pt-5">
-                Check Availability
-              </div>
-              <div className="font-light pt-4">
-                Get ready for the trip of a lifetime
-              </div>
-              <div className="pt-5">
-                <Button label="Book" outlined />
-              </div>
-            </div>
-          </div> */}
+        <Bookings />
+        <div className="surface-0">
+            <div className="grid justify-content-around overflow-hidden pt-5">
+                <ImageButton 
+                    imgUrl={"https://assets.website-files.com/64be07bb1f833c7c8821b67c/64bfda83ce7792ff7034126a_DeluxeRoom.jpg"}
+                    heading={"£300/night"}
+                    subHeading={"Deluxe room"}
+                /> 
+                <ImageButton 
+                    imgUrl={"https://assets.website-files.com/64be07bb1f833c7c8821b67c/64bfe055d6a432f7de96be21_PenthouseSuite.jpg"}
+                    heading={"£450/night"}
+                    subHeading={"Ocean view suite"}
+                /> 
+                <ImageButton 
+                    imgUrl={"https://assets.website-files.com/64be07bb1f833c7c8821b67c/64bfdca47f54ac3ff269a282_PremierSuite2.jpg"}
+                    heading={"£670/night"}
+                    subHeading={"Premier suite"}
+                /> 
 
-          <div className="col-12 md:col-6 lg:col-6 pt-5">
-            <div className="text-center text-6xl pt-5 about">
-              A dream for all your senses with amazing views of the ocean
-            </div>
-          </div>
-
-          <div className="col-12 md:col-6 lg:col-12 pt-5">
-            <div className="surface-0 background-image">
-              <div className="p-3 bg-white font-bold award-winning">
-                <div className="text-container">
-                  <h5 className="rooftop-heading ">ROOFTOP</h5>
-                  <h2 className="rooftop-title">AWARD WINNING PARADISE</h2>
-                  <p className="rooftop-text">
-                    At our Sea View Rooftop, indulge in panoramic ocean vistas.
-                    Savor drinks with the sea as your backdrop, embracing
-                    tranquility and natural beauty.
-                  </p>
+            <div className="col-12 md:col-6 lg:col-6 pt-5">
+                <div className="text-center text-6xl pt-5 about">
+                A dream for all your senses with amazing views of the ocean
                 </div>
-              </div>
             </div>
-            <div className="surface-0 background-image">
-              <div className="text-center p-3 bg-primary font-bold"></div>
+
+            <div className="col-12 md:col-6 lg:col-12 pt-5">
+                <div className="surface-0 background-image">
+                <div className="p-3 bg-white font-bold award-winning">
+                    <div className="text-container">
+                    <h5 className="rooftop-heading ">ROOFTOP</h5>
+                    <h2 className="rooftop-title">AWARD WINNING PARADISE</h2>
+                    <p className="rooftop-text">
+                        At our Sea View Rooftop, indulge in panoramic ocean vistas.
+                        Savor drinks with the sea as your backdrop, embracing
+                        tranquility and natural beauty.
+                    </p>
+                    </div>
+                </div>
+                </div>
+                <div className="surface-0 background-image">
+                <div className="text-center p-3 bg-primary font-bold"></div>
+                </div>
             </div>
-          </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
--- src/components/bookings/Bookings.jsx
+++ src/components/bookings/Bookings.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
 import { Button } from 'primereact/button';
 
+import { Calendar } from 'primereact/calendar';
+import { Dropdown } from 'primereact/dropdown';
+
+
+
 function Bookings () {
+    const [date, setDate] = useState(null);
+
+    const [selectedNumberNights, setSelectedNumberNights] = useState(null);
+    const nights = [
+        { name: '1 night', code: '1' },
+        { name: '2 nights', code: '2' },
+        { name: '3 nights', code: '3' },
+        { name: '4 nights', code: '4' },
+        { name: '5 nights', code: '5' }
+    ];
+
+    const [selectedRoom, setSelectedRoom] = useState(null);
+    const rooms = [
+        { name: 'Deluxe room', code: 'DR' },
+        { name: 'Ocean view suite', code: 'OS' },
+        { name: 'Premier suite', code: 'PS' },
+    ];
+
     return (
-        <div className="booking-section">
-            Book now<Button label="Book" icon="pi pi-check" />
+        <div className="flex justify-content-center booking-section">
+            <div className="card flex justify-content-center check-in mt-5 mr-5">
+                <Calendar placeholder="Check-in Date" id="buttondisplay" value={date} onChange={(e) => setDate(e.value)} showIcon />
+            </div>
+            <div className="card flex justify-content-center mt-5 mr-5">
+                <Dropdown value={selectedNumberNights} onChange={(e) => setSelectedNumberNights(e.value)} options={nights} optionLabel="name" 
+                    placeholder="Number of nights" className="w-full md:w-14rem" />
+            </div>
+            <div className="card flex justify-content-center mt-5 mr-5">
+                <Dropdown value={selectedRoom} onChange={(e) => setSelectedRoom(e.value)} options={rooms} optionLabel="name" 
+                    placeholder="Rooms" className="w-full md:w-14rem" />
+            </div>  
+            <div className="card flex justify-content-center mt-5 mr-5">
+                <Button type="submit" label="Submit" />
+            </div>
+            
         </div>
     )
 }
